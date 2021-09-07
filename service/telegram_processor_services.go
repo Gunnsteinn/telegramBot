8e6fddff867e1bf6002b64930ae197f0911b88b0
Guillermo Colombero <guillecolo@gmail.com>
2021-09-07 17:41:02 -0300

    Update telegram_processor_services.go

--- service/telegram_processor_services.go
+++ service/telegram_processor_services.go
@@ -55,6 +55,14 @@ func TelegramProcessorService(webhookReqBody domain.WebhookReqBody) (*domain.Sen
 // sayPolo takes a chatID and sends "polo" to them
 func sendMessage(chatID int64, chatText string) error {
 	// Create the request body struct
+		chatText = "*bold *text* _italic *text_ __underline__ " +
+		"~strikethrough~ *bold _italic bold " +
+		"~italic bold strikethrough~ __underline italic bold___ bold* [inline URL](http://www.example.com/) " +
+		"[inline mention of a user](tg://user?id=123456789) " +
+		"`inline fixed-width code` " +
+		"```pre-formatted fixed-width code block``` ```python " +
+		"pre-formatted fixed-width code block written in the Python programming language" +
+		"```"
 	reqBody := domain.SendMessageReqBody{
 		ChatID: chatID,
 		Text:   chatText,
