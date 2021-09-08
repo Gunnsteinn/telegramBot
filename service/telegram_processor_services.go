3b35ec270e16de2a976fbfa8df61c7dcf4963e5f
Gunnsteinn <guillecolo@gmail.com>
2021-09-08 10:20:38 -0300

    Add HTML in chat text.

--- service/telegram_processor_services.go
+++ service/telegram_processor_services.go
@@ -42,7 +42,7 @@ func TelegramProcessorService(webhookReqBody domain.WebhookReqBody) (*domain.Sen
 	}
 	// log a confirmation message if the message is sent successfully
 	fmt.Println("reply sent" + string(sponsorInfo.Body))
-	sendMessage(webhookReqBody.Message.Chat.ID, "")
+	sendMessage(webhookReqBody.Message.Chat.ID, textGenerator(sponsorInfo.Body))
 
 	return nil, nil
 }
@@ -50,23 +50,6 @@ func TelegramProcessorService(webhookReqBody domain.WebhookReqBody) (*domain.Sen
 // sayPolo takes a chatID and sends "polo" to them
 func sendMessage(chatID int64, chatText string) error {
 	// Create the request body struct
-	chatText = `- Negrita: <b>texto en negrita</b>, <strong>negrita</strong>
-				- Cursiva: <i>texto en cursiva</i>, <em>cursiva</em>
-
-				- Subrayado: <u>texto subrayado</u>, <ins>subrayado</ins>
-
-				- Tachado: <s>texto tachado</s>, <strike>tachado</strike>, <del>tachado</del>
-
-				Enlaces: <a href="https://www.Tecnonucleous.com/">Tecnonucleous</a>
-
-				<a href="https://www.carspecs.us/photos/c8447c97e355f462368178b3518367824a757327-2000.jpg"> </a>
-
-				<a href="https://storage.googleapis.com/assets.axieinfinity.com/axies/3624156/axie/axie-full-transparent.png">&#8205;</a>
-
-				<a href="tg://user?id=123456789">inline mention of a user</a>
-				- Código: <code>texto con el código</code>
-
-				- Indica a la API que debe respetar los saltos de línea y los espacios en blanco: <pre>Texto con saltos de línea y espacios</pre>`
 
 	reqBody := domain.SendMessageReqBody{
 		ChatID:    chatID,
@@ -91,4 +74,24 @@ func sendMessage(chatID int64, chatText string) error {
 	}
 
 	return nil
+}
+
+func textGenerator(sponsorInfo []byte) string {
+	var sponsor domain.Sponsor
+	json.Unmarshal(sponsorInfo, &sponsor)
+
+	chatText := `- Negrita: <b>texto en negrita</b>, <strong>negrita</strong>
+				- Cursiva: <i>texto en cursiva</i>, <em>cursiva</em>
+
+				- Subrayado: <u>texto subrayado</u>, <ins>subrayado</ins>
+
+				- Tachado: <s>texto tachado</s>, <strike>tachado</strike>, <del>tachado</del>
+
+				Enlaces: <a href="https://storage.googleapis.com/assets.axieinfinity.com/axies/3624156/axie/axie-full-transparent.png">Tecnonucleous</a>
+
+				- Código: <code>texto con el código</code>
+
+				- Indica a la API que debe respetar los saltos de línea y los espacios en blanco: <pre>Texto con saltos de línea y espacios</pre>`
+
+	return chatText
 }
